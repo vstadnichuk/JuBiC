1ba8505accc4e510e8b1a4fdf8ce0db00a94e093
cwieber <cedrick.wieber@rwth-aachen.de>
2025-12-12 12:52:43 +0100

    Implemented export of GBC and MibS instance to MibS input file format
    - Added 'output_GBC_instance()' for printing GBC instances
    - Added 'output_MibS_instance()' for printing MibS instances

--- src/mibs_parser/mibs_input_reader.jl
+++ src/mibs_parser/mibs_input_reader.jl
@@ -3,5 +3,6 @@ include("mibs_mps_reader.jl")
 include("mibs_instance_builder.jl")
 include("mibs_instance_gbc_builder.jl")
 include("mibs_transformation.jl")
+include("mibs_instance_to_file_input.jl")
 
-export get_MibS_instance, get_GBC_instance, transform_GBC_to_MibS
+export get_MibS_instance, get_GBC_instance, transform_GBC_to_MibS, output_MibS_instance, output_GBC_instance
